b41a3f2a71f73521c40006053011496b6c2b6b53
Thyago Biagio <thybiagio@gmail.com>
2024-06-28 11:45:42 -0300

    Listagem de repositórios

--- js/app.js
+++ js/app.js
@@ -8,3 +8,25 @@ links.forEach(link => {
         link.style.textDecoration = 'none';
     });
 });
+
+function fetchGitHubRepos(thybiagio) {
+    fetch(`https://api.github.com/users/${thybiagio}/repos`)
+        .then(response => response.json())
+        .then(data => {
+            const repoList = document.getElementById('repo-list');
+            data.forEach(repo => {
+                const listItem = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = `https://${thybiagio}.github.io/${repo.name}`;
+                link.textContent = repo.name;
+                link.target = '_blank';
+                listItem.appendChild(link);
+                repoList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchGitHubRepos('thybiagio'); 
+});
